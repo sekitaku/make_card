be8c0e3e012b0aad134345e684c0d728f467754c
sekitaku <48849918+sekitaku@users.noreply.github.com>
2019-04-11 16:52:48 +0900

    Update make_card.py
    idからuserオブジェクトが取得できない例外をキャッチして対象の行をno_icon.csvとして出力

--- make_card.py
+++ make_card.py
@@ -19,6 +19,8 @@ print("CSVファイルの名前を入力してください")
 csvfile = input()
 reader_list = []
 id_list = []
+no_id_line = []
+no_id_list = []
 with open(csvfile,'r') as f:
     reader_list = list(csv.reader(f))
     id_row = 0
@@ -45,12 +47,19 @@ except FileExistsError as e:
     sys.exit()
 
 print("アイコン画像の取得を開始")
+line_num = 1
 icon_num = 1
 for id in id_list:
     if '@' in id:
         id = id.lstrip('@')
     print(str(icon_num) + ':' + id)
-    user = api.get_user(id)
+    try:
+        user = api.get_user(id)
+    except tweepy.error.TweepError:
+        print(str(line_num) + "行目の画像取得に失敗")
+        no_id_line.append(line_num)
+        line_num += 1
+        continue
     url = user.profile_image_url_https.replace("_normal.", ".")
     other,ext = os.path.splitext(url)
     if not ext:
@@ -62,6 +71,7 @@ for id in id_list:
             f.write(data)
     except urllib.error.URLError as e:
         print(e)
+    line_num += 1
     icon_num += 1
 print("アイコン画像の取得完了")
 
@@ -71,7 +81,16 @@ tour_name = input()
 reader_list[0].append("大会名")
 for i in range(1, len(reader_list)):
     reader_list[i].append(tour_name)
+if len(no_id_line) > 0:
+    no_id_list.append(reader_list[0])
+    for i in no_id_line:
+        no_id_list.append(reader_list.pop(i))
 with open("./input.csv", 'w') as f:
     writer = csv.writer(f)
     writer.writerows(reader_list)
 print("input.csvの出力完了")
+if len(no_id_list) > 0:
+    with open("./no_icon.csv", 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(no_id_list)
+    print("アイコン画像を取得できなかった行をno_icon.csvとして出力しました")
